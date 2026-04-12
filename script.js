b24154cc25a9ad6e5ab26e082bd2767ec2236b55
Payal-Phougat <phougatpayal3@gmail.com>
2026-04-12 20:56:44 +0530

    Added dark mode

--- script.js
+++ script.js
@@ -100,6 +100,28 @@ const addToFavorites = (movie) => {
   alert("Added to Favorites ❤️");
 };
 
+// dark mode 
+const themeToggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if(localStorage.getItem("theme") === "dark"){
+  document.body.classList.add("dark");
+}
+
+// Toggle theme
+
+themeToggle.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark");
+
+  if(document.body.classList.contains("dark")){
+    localStorage.setItem("theme","dark");
+  } else {
+    localStorage.setItem("theme","light");
+  }
+
+});
+
 
 // Call Function
 getTrendingVideos();
